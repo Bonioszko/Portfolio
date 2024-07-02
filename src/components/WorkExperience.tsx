5741fb06859152b599697d7563a41f25d6f3176a
Bonioszko <bartek.pers@gmail.com>
2024-07-02 13:33:30 +0200

    second work experience

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -8,10 +8,27 @@ function WorkExperience() {
             <Section left={true} id="work-experience">
                 <SectionHeading text="Work Experience"></SectionHeading>
                 <div className="flex flex-col gap-2">
+                    <h2 className="text-center text-2xl  bg-gradient-to-r to-lime-300 from-green-400 text-primary-color font-bold p-2 rounded-xl">
+                        Bright Coders's Factory - Full Stack Developer
+                    </h2>
+                    <h1 className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-lime-300 to-green-400 pb-3 text-center">
+                        July 2024- present
+                    </h1>
+                    <h1 className="text-3xl text-center text-text-color">
+                        My responsibilities
+                    </h1>
+
+                    <ul className="gap-4 flex flex-col">
+                        <SingleResonsibility text="Developing apps in the MERN stack "></SingleResonsibility>
+                    </ul>
                     <h2 className="text-2xl  bg-gradient-to-r to-lime-300 from-green-400 text-primary-color font-bold p-2 rounded-xl">
                         Air Spiralo Poland - Information Communication
                         Technology Support
                     </h2>
+                    <h1 className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-lime-300 to-green-400 pb-3 text-center">
+                        December 2022 - June 2024
+                    </h1>
+
                     <h1 className="text-3xl text-center text-text-color"></h1>
                     <h1 className="text-3xl text-center text-text-color">
                         My responsibilities
